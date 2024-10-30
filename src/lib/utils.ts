078d85cd75443914b3fcbcee883ab5f4c4b55a08
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2024-10-30 22:44:20 +0330

    feat: Write functions to get the url of the website and api url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { env } from "@/env";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export const getBaseUrl = () =>
+  env.NODE_ENV === "development"
+    ? `http://localhost:3000`
+    : `https://${env.BASE_URL}`;
+
+export function getApiUrl() {
+  if (typeof window !== "undefined") return "";
+
+  return `${getBaseUrl()}/api/trpc`;
 }
